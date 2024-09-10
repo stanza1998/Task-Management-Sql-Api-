d94da118ca9ef271a8e38f609fca9587c51e71f2
stanza1998 <narib98jerry@gmail.com>
2024-09-10 11:54:17 +0200

    update

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const taskRoute = require('./routes/tasks');
 app.use(express.json());
 app.use(cors());
 
+
+app.get('/', (req, res) => {
+    res.send('Hello from Render!');
+});
 // Ensure this path starts with a leading slash
 app.use('/tasks', taskRoute);
 
